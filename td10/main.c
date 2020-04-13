9ed92abf6e6fc2223c2ef51f9b133694e27dc547
m27bay <mickael.ledenmat@gmail.com>
2020-04-13 14:52:55 +0200

    Add enother test

--- td10/main.c
+++ td10/main.c
@@ -8,8 +8,11 @@ int main(int argc, char const *argv[])
   // Test mutex with ten threads
   // mutex1(10);
 
-  // test barrier with 3 threads
-  barrier1(3);
+  // Test barrier with 3 threads
+  // barrier1(3);
+
+  // Test barrier with 10 threads
+  barrier1(10);
 
   // progam's end
   return 0;
