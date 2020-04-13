36468a5c1ac45d31b8626609ef0ff6eb8bd04348
m27bay <mickael.ledenmat@gmail.com>
2020-04-13 14:19:49 +0200

    Add new function test, 'barrier1()

--- td10/main.c
+++ td10/main.c
@@ -3,10 +3,13 @@
 int main(int argc, char const *argv[])
 {
   // Test mutex with three threads
-  mutex1(3);
+  // mutex1(3);
 
   // Test mutex with ten threads
-  mutex1(10);
+  // mutex1(10);
+
+  // test barrier with 3 threads
+  barrier1(3);
 
   // progam's end
   return 0;
